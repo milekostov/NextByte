8cc0efb0c5abf40b3e55585f3547902b06a2ac66
Mile Kostov <milekostov@t.mk>
2024-11-13 22:38:44 +0100

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+  emailjs.init("IUSVwxdals_-cYv7o");
+});
+
+function sendEmail(event) {
+  event.preventDefault();
+
+  const fName = document.getElementById("fName").value;
+  const lName = document.getElementById("lName").value;
+  const email = document.getElementById("email").value;
+  const phone = document.getElementById("phone").value;
+  const message = document.getElementById("message").value;
+  const sendButton = document.querySelector("button");
+  const successMessage = document.getElementById("success-message");
+
+  const emailParams = {
+    from_name: `${fName} ${lName}`,
+    from_email: email,
+    phone: phone,
+    message: message,
+  };
+
+  emailjs.send("service_4o85ll8", "template_oq02nog", emailParams).then(
+    function (response) {
+      successMessage.textContent = "The message has been successfully sent!";
+      sendButton.disabled = true;
+      sendButton.style.backgroundColor = "#d3d3d3";
+    },
+    function (error) {
+      alert("Failed to send email: " + error.text);
+    }
+  );
+}
